8190065b1fa146f48724a540f26a9a1883d90eca
Sky Walker <Miles.Eddy1@Gmail.com>
2019-07-11 08:37:36 -0600

    broke code server not responding ?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,164 @@
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
-  
+var friends = [{
+        "name": "Missy ",
+        "photo": "https://media.lihttps://upload.wikimedia.org/wikipedia/commons/7/77/Missy_Elliot.jpgcdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "50 Cent ",
+        "photo": "http://www.brooklynvegan.com/files/2019/02/50cent.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Bill",
+        "photo": "https://specials-images.forbesimg.com/imageserve/5c76b4b84bbe6f24ad99c370/416x416.jpg?background=000000&cropX1=0&cropX2=4000&cropY1=0&cropY2=4000",
+        "scores": [
+            5,
+            1,
+            2,
+            2,
+            4,
+            2,
+            3,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Elon ",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Elon_Musk_Royal_Society.jpg/440px-Elon_Musk_Royal_Society.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            3,
+            3,
+            2,
+            5,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Jay Z",
+        "photo": "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/mufgpxz0jzyiisw7jgry/jay-z",
+        "scores": [
+            2,
+            3,
+            3,
+            3,
+            3,
+            3,
+            4,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Beyonce ",
+        "photo": "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/09/04/13/beyonce.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            3,
+            3,
+            3,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Will",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzQzMTY2NDc4/will-smith-9542165-1-402.jpg",
+        "scores": [
+            5,
+            2,
+            3,
+            4,
+            2,
+            1,
+            1,
+            1,
+            1,
+            3
+        ]
+    },
+    {
+        "name": "Lewis",
+        "photo": "https://www.formula1.com/content/fom-website/en/drivers/lewis-hamilton/_jcr_content/image.img.1024.medium.jpg/1554818913486.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            1,
+            1,
+            2,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Monica",
+        "photo": "https://cdn.singersroom.com/wp-content/uploads/2019/01/monica-commitment-single-2.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            2,
+            5,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Seinfeld ",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2014/11/26/26-jerry-seinfeld.w330.h412.2x.png",
+        "scores": [
+            3,
+            4,
+            4,
+            4,
+            5,
+            5,
+            3,
+            2,
+            3,
+            4
+        ]
+    }
+
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require("express");
-
+var path = require('path');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+var friends = require('./app/data/friends.js');
+var server = http.createServer(handleRequest);
+var http = require("http");
+var fs = require("fs");
 
+function handleRequest(req, res) {
 
-
-
+    // Here we use the fs package to read our index.html file
+    fs.readFile(__dirname + "/home.html", function(err, data) {
+      if (err) throw err;
+      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+      // an html file.
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+  
 // Starts our server.
 server.listen(PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
